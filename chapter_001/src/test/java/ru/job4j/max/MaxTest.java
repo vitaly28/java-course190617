80acaf3066639937cb8f2ba33575802e3939a092
Vitaly Berstenev <sm_plus82@mail.ru>
2017-06-19 17:03:32 +0400

    change MaxTest vitaly

--- chapter_001/src/test/java/ru/job4j/max/MaxTest.java
+++ chapter_001/src/test/java/ru/job4j/max/MaxTest.java
@@ -1,18 +1,20 @@
 package ru.job4j.max;
 
+
 import org.junit.Test;
 
 import static org.hamcrest.core.Is.is;
-import static org.junit.Assert.assertThat;
+import static org.junit.Assert.*;
+
 
 public class MaxTest {
-    @Test
-    public void () {
-        Max compare = new Max();
-        compare.max(2D, 1D);
-        int result = compare.getResult();
-        int expected = 2D;
-        assertThat(result, is(expected));
+@Test
+    public void MaxOfTwoNunber(){
+    Max dog = new Max();
+    int number1 = 1;
+    int number2 = 2;
+    int expected = 2;
+    int result = dog.max(number1,number2);
+assertThat(result,is(expected));
     }
-        
 }
